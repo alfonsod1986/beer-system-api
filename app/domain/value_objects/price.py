9596ea026cebbbde575e841b8a94a16cdbc4c2f6
Luis Alfonso Delgado Ballado <alfonsod18@gmail.com>
2024-11-12 21:07:17 -0600

    chore: Add Price value object (#1)

---
+++ app/domain/value_objects/price.py
@@ -0,0 +1,14 @@
+class Price:
+  def __init__(self, value: float):
+    if not isinstance(value, float) or value <= 0:
+      raise ValueError("Price must be a positive float")
+    self.__value = value
+  
+  @property
+  def value(self):
+    return self.__value
+  
+  def __eq__(self, other):
+    if isinstance(other, Price):
+      return self.__value == other.__value
+    return False
