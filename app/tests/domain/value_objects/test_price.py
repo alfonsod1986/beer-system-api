501d4e0eb2f27f3921f2db3f57d3733ae7f0c2d8
alfonsod1986 <alfonsod18@gmail.com>
2024-11-12 21:04:41 -0600

    chore: Add Price value object

---
+++ app/tests/domain/value_objects/test_price.py
@@ -0,0 +1,21 @@
+import pytest
+from app.domain.value_objects.price import Price
+
+def test_price_positive_float_value():
+  price = Price(value=15.99)
+  assert price.value == 15.99
+
+@pytest.mark.parametrize("test_input,expected",[
+  (0, "Price must be a positive float"),
+  (-15, "Price must be a positive float"),
+])
+def test_price_invalid_value(test_input, expected):
+  with pytest.raises(ValueError) as exc_info:
+    Price(value=test_input)
+  assert str(exc_info.value) == expected
+
+def test_price_equality():
+  price_1 = Price(value=11.45)
+  price_2 = Price(value=11.45)
+
+  assert price_1 == price_2
