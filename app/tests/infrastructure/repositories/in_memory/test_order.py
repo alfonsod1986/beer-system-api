3755012d3d1649a86df1c95e7d41ae4f21b2aafa
alfonsod1986 <alfonsod18@gmail.com>
2024-11-14 15:41:54 -0600

    chore: Add OrderInMemoryRepository - get_by_id

--- app/tests/infrastructure/repositories/in_memory/test_order.py
+++ app/tests/infrastructure/repositories/in_memory/test_order.py
@@ -1,5 +1,6 @@
 import pytest
 from app.infrastructure.repositories.in_memory.order import OrderInMemoryRepository
+from app.domain.entities.order import OrderEntity
 
 
 @pytest.fixture
@@ -19,3 +20,15 @@ def test_load_data_from_file_with_default_value():
   orders = repository.get_all()
 
   assert len(orders) == 0
+
+def test_get_order_by_id(order_in_memory_repository, domain_orders):
+  order = order_in_memory_repository.get_by_id(1)
+
+  first, *_ = domain_orders
+
+  assert isinstance(order, OrderEntity) == True
+  assert order.id == 1
+  assert order == first
+  order = order_in_memory_repository.get_by_id(99)
+
+  assert order is None
