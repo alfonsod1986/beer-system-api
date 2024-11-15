18299cf8b51b3c97f8c7326ecabf0b076b6c172c
Luis Alfonso Delgado Ballado <alfonsod18@gmail.com>
2024-11-15 09:29:30 -0600

    chore: Add get order by id route (#23)

--- app/infrastructure/rest/fastapi/v1/routes/order.py
+++ app/infrastructure/rest/fastapi/v1/routes/order.py
@@ -20,3 +20,17 @@ def get_all_orders(
     return [order.to_dict() for order in orders]
   except ValueError as e:
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+@router.get('/{order_id}', response_model=OrderOutput)
+@inject
+def get_order_by_id(
+  order_id: int,
+  order_service: OrderService = Depends(Provide[Container.order_service])
+):
+  try:
+    order = order_service.get_by_id(order_id)
+    if not order:
+      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Order with ID {order_id} not found")
+    return order.to_dict()
+  except ValueError as e:
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
